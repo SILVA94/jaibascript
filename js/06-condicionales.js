80fa373c375479eaec5334d32999a6807b9575ef
Ricardo Silva <ric.silva15@gmail.com>
2023-03-04 02:00:35 -0600

    condicional if

---
+++ js/06-condicionales.js
@@ -0,0 +1,14 @@
+'use strict'
+
+//Condicional IF
+// Si A es igual a B entonces haz algo
+
+var edad1 = 10;
+var edad2 = 12;
+//Si pasa esto
+if(edad1 > edad2){
+    console.log("Edad uno es mayor que edad2")
+    //ejecuta esto
+}else {
+    console.log("La edad1 es inferior")
+}
