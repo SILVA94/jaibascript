01d7eea381f96b935d920f66ed3e0211d30013e6
Ricardo Silva <ric.silva15@gmail.com>
2023-03-09 00:55:37 -0600

    subiendo archivo 06-condicionales.js

--- js/06-condicionales.js
+++ js/06-condicionales.js
@@ -3,12 +3,58 @@
 //Condicional IF
 // Si A es igual a B entonces haz algo
 
-var edad1 = 10;
-var edad2 = 12;
-//Si pasa esto
-if(edad1 > edad2){
-    console.log("Edad uno es mayor que edad2")
-    //ejecuta esto
-}else {
-    console.log("La edad1 es inferior")
+var edad = 71;
+var nombre = 'David Suarez';
+/* operadores relacionales
+    Mayor: >
+    Menor: <
+    Mayor o igual: >=
+    Menor o igual: <=
+    Igual: ==
+    Distinto: =
+
+*/
+
+if (edad >= 18){
+    console.log(nombre+" tiene " +edad+" a;os, es mayor de edad")
+    if(edad == 33){
+        console.log('Todavia eres milenial')
+    }else if(edad >= 70){
+        console.log('Eres anciano')
+    } else{
+        console.log('Ya no eres milenial')
+    }
+} else{
+    console.log(nombre+" tiene " +edad+" a;os, es menor de edad")
 }
+
+/*
+ Operadores logicos
+ AND(Y): &&
+ OR(O) ||
+ Negacion: !
+ */
+
+ var year = 2008
+
+//NEGACION
+ if(year != 2016){
+    console.log("El a;o no es 2016, realmente es: "+year)
+ }
+
+ //AND
+
+ if(year >= 2000 && year <= 2020){
+    console.log("Estamos en la era actual")
+ } else{
+    console.log("Estamos en la era post moderna")
+ }
+
+ //OR
+
+ if(year == 2008 || (year >= 2018 && year == 2028)){
+    console.log("El a;o acaba en 8")
+ }else{
+    console.log("A:O NO REGISTRADO")
+ }
+
